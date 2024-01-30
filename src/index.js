384bbb9678228380662f49379604e25a10017ea4
shahbaz yasin <shahbazyasin26@gmail.com>
2024-01-30 10:29:11 -0800

    my app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  <>
+  <h1>Hello World!</h1>
+  <p>This is my first react app.</p>
+  </>,
   document.getElementById("root")
 );
